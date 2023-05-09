788d4745bf0f9b03ce9a18a4ea4c76b3d47a913f
Sawan <savan.visavadia@drcsystems.com>
2023-05-09 17:15:31 +0530

    Add all my files

---
+++ app/Http/Controllers/ReminderController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\reminder;
+use Illuminate\Http\Request;
+
+class ReminderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\reminder  $reminder
+     * @return \Illuminate\Http\Response
+     */
+    public function show(reminder $reminder)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\reminder  $reminder
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(reminder $reminder)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\reminder  $reminder
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, reminder $reminder)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\reminder  $reminder
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(reminder $reminder)
+    {
+        //
+    }
+}
